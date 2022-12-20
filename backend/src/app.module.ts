38098333f5407af945233f831fc11f41ea3e7c00
Meriam Gaied <meriamgaied9@gmail.com>
2022-12-20 10:02:18 +0100

    config typeOrm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,29 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { CampingSiteModule } from './camping_site/camping_site.module';
 import { BookingModule } from './booking/booking.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import * as dotenv from 'dotenv'
+
+dotenv.config() ;
 
 @Module({
-  imports: [UserModule, CampingSiteModule, BookingModule],
+  imports: [UserModule, CampingSiteModule, BookingModule,
+  TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: process.env.DB_HOST,
+    port:parseInt(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password:process.env.DB_PASSWORD ,
+    database: process.env.DB_NAME,
+    autoLoadEntities:true,
+    logging:true,
+    synchronize: true,
+    entities: [] ,
+  }),
+  ConfigModule.forRoot(
+    {isGlobal: true} 
+  )],
   controllers: [AppController],
   providers: [AppService],
 })
